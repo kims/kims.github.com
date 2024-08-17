4d3c6d8df7dfae32fa98ca5e5c87df179a894ecf
kims <kims>
2024-08-17 21:10:14 +0200

    mat

---
+++ push.sh
@@ -0,0 +1,3 @@
+GIT_SSH_COMMAND='ssh -i $HOME/.ssh/id_ed25519 -o IdentitiesOnly=yes -F /dev/null' git add .
+GIT_SSH_COMMAND='ssh -i $HOME/.ssh/id_ed25519 -o IdentitiesOnly=yes -F /dev/null' git commit -m "mat"
+GIT_SSH_COMMAND='ssh -i $HOME/.ssh/id_ed25519 -o IdentitiesOnly=yes -F /dev/null' git push
